7eed3866a3a29147f69874336c9cf4b22ab92c01
ArgEric8 <55736566+ArgEric8@users.noreply.github.com>
2020-01-09 15:42:29 +0100

    Första metoden

--- ConverterProgram/Program.cs
+++ ConverterProgram/Program.cs
@@ -17,14 +17,8 @@ namespace ConverterProgram
             while (choice != 2)
             {
                 Console.WriteLine("\nVälj ett alternativ");
-<<<<<<< HEAD
-                Console.WriteLine("1. Time / distance / speed");
-                Console.WriteLine("2. Gallons");
-                Console.WriteLine("3. Avsluta");
-=======
                 Console.WriteLine("1. Hej");
                 Console.WriteLine("2. Avsluta");
->>>>>>> e5c2f13b8c597ec702e4b61fb217da3df75a1812
                 //choice = Convert.ToInt32(Console.ReadLine());
 
                 if (int.TryParse(Console.ReadLine(), out choice))
@@ -32,49 +26,7 @@ namespace ConverterProgram
                     // Första valet 
                     if (choice == 1)
                     {
-<<<<<<< HEAD
-                        // ConverterService testrun = new ConverterService();
-=======
->>>>>>> e5c2f13b8c597ec702e4b61fb217da3df75a1812
-                        float distance;
-                        float speed;
-                        Console.WriteLine("\nVälj");
-                        Console.WriteLine("1. Meter / sekund");
-                        Console.WriteLine("2. Kilometer / timme");
-                        int choiceFormat = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine();
-
-                        // Meter / Sekund
-                        if (choiceFormat == 1)
-                        {
-                            Console.WriteLine("Hur många meter?");
-                            distance = float.Parse(Console.ReadLine());
-                            Console.WriteLine("Hur många meter per sekund?");
-                            speed = float.Parse(Console.ReadLine());
-
-<<<<<<< HEAD
-                            Console.WriteLine("Sekunder: {0:0.##}",converter.CalculateTravelTime(speed, distance));
-                            Console.WriteLine("Minuter: {0:0.##}", converter.CalculateTravelTime(speed, distance)/60);
-                            Console.WriteLine("Timmar: {0:0.##}", converter.CalculateTravelTime(speed, distance)/60/60);
-=======
-                            Console.WriteLine("Sekunder: {0:0.##}", converter.CalculateTravelTime(speed, distance));
->>>>>>> e5c2f13b8c597ec702e4b61fb217da3df75a1812
-                        }
-
-                        // Kilometer / Timme
-                        else if (choiceFormat == 2)
-                        {
-<<<<<<< HEAD
-                            Console.WriteLine("Hur många kilometer?");
-                            distance = float.Parse(Console.ReadLine());
-                            Console.WriteLine("Hur många kilometer per timme?");
-                            speed = float.Parse(Console.ReadLine());
-
-                            Console.WriteLine("Svar: {0:0.0#} timmar", converter.CalculateTravelTime(speed, distance));
-=======
-                            Console.WriteLine("");
->>>>>>> e5c2f13b8c597ec702e4b61fb217da3df75a1812
-                        }
+                        
                     }
 
                     // Andra valet
@@ -97,9 +49,55 @@ namespace ConverterProgram
             }
         }
 
-        public showTimeDistanceSpeedMenu();
+        public void showTimeDistanceSpeedMenu()
         {
+            ConverterService converter = new ConverterService();
 
+            float distance;
+            float speed;
+            Console.WriteLine("\nVälj");
+            Console.WriteLine("1. Km / h");
+            Console.WriteLine("2. Distans??");
+            Console.WriteLine("3. Tid??");
+            Console.WriteLine("4. Återvänd");
+            Console.WriteLine("5. Avsluta");
+            int choiceFormat = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // km / h
+            if (choiceFormat == 1)
+            {
+                Console.WriteLine("Hur många meter?");
+                distance = float.Parse(Console.ReadLine());
+                Console.WriteLine("Hur många meter per sekund?");
+                speed = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Sekunder: {0:0.##}", converter.CalculateTravelTime(speed, distance));
+            }
+
+            // distans == ????
+            if (choiceFormat == 2)
+            {
+                
+            }
+
+            // tid ?????????
+            if (choiceFormat == 3)
+            {
+                
+            }
+
+            // Återvänd
+            if (choiceFormat == 4)
+            {
+                Console.WriteLine("Återvänd");
+            }
+
+            // Avsluta
+            else if (choiceFormat == 5)
+            {
+                Console.WriteLine("Avsluta");
+            }
         }
     }
 }
